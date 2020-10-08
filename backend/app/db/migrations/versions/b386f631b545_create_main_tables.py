a71165c385cb0ca2e0c32caca2e9e119cc0aa013
kosorz <kosorz.artur@gmail.com>
2020-10-08 18:59:21 +0200

    Setup migrations

---
+++ backend/app/db/migrations/versions/b386f631b545_create_main_tables.py
@@ -0,0 +1,37 @@
+"""create main tables
+
+Revision ID: b386f631b545
+Revises: 
+Create Date: 2020-10-08 03:11:21.195565
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic
+revision = 'b386f631b545'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def create_cleanings_table() -> None:
+    op.create_table(
+        "cleanings",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.Text, nullable=False, index=True),
+        sa.Column("description", sa.Text, nullable=True),
+        sa.Column("cleaning_type",
+                  sa.Text,
+                  nullable=False,
+                  server_default="spot_clean"),
+        sa.Column("price", sa.Numeric(10, 2), nullable=False),
+    )
+
+
+def upgrade() -> None:
+    create_cleanings_table()
+
+
+def downgrade() -> None:
+    op.drop_table("cleanings")
